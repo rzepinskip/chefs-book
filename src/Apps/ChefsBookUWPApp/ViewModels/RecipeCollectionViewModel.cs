9ec0c8ed01e34acf8ef6ba351b8d2954f80d92ef
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2017-10-28 22:57:16 +0200

    Add basic ViewModels
    Related Work Items: #124

---
+++ src/Apps/ChefsBookUWPApp/ViewModels/RecipeCollectionViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GalaSoft.MvvmLight;
+using System.Collections.ObjectModel;
+
+namespace ChefsBook_UWP_App.ViewModels
+{
+    public class RecipeCollectionViewModel : ViewModelBase
+    {
+        private ObservableCollection<RecipeViewModel> _recipes;
+        public ObservableCollection<RecipeViewModel> Recipes
+        {
+            get => _recipes;
+            set => Set(() => Recipes, ref _recipes, value);
+        }
+    }
+}
