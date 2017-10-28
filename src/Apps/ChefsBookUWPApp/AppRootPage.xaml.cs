e43a3090a77a8973a24372a55dd6d16ec751c125
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2017-10-29 00:23:16 +0200

    Add app navigation and menu shell
    Related Work Items: #128

---
+++ src/Apps/ChefsBookUWPApp/AppRootPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ChefsBook_UWP_App.Views;
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ChefsBook_UWP_App
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AppRootPage : Page
+    {
+        public AppRootPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void NavigationView_Loaded(object sender, RoutedEventArgs e)
+        {
+            contentFrame.Navigate(typeof(HomePage));
+        }
+
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (!args.IsSettingsSelected)
+            {
+                var selectedItem = (NavigationViewItem)args.SelectedItem;
+                string pageName = "ChefsBook_UWP_App.Views." + ((string)selectedItem.Tag);
+                Type pageType = Type.GetType(pageName);
+                contentFrame.Navigate(pageType);
+            }
+        }
+    }
+}
