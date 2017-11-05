901f8755090fb0cd1d2c179702e211768c782f88
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2017-11-05 11:49:18 +0100

    Add basic data template for recipe tile
    Related Work Items: #118

--- src/Apps/ChefsBookUWPApp/Services/FakeRecipeApiService.cs
+++ src/Apps/ChefsBookUWPApp/Services/FakeRecipeApiService.cs
@@ -18,7 +18,7 @@ namespace ChefsBook_UWP_App.Services
                     Id = Guid.NewGuid(),
                     Title = "Moule the crema with oreiv lemoinaie",
                     Description = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-                    Image = "",
+                    Image = @"/Assets/seafood_dish.jpg",
                     Duration = TimeSpan.FromMinutes(15),
                     Servings = 4,
                     Notes = @"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. "
@@ -28,7 +28,7 @@ namespace ChefsBook_UWP_App.Services
                     Id = Guid.NewGuid(),
                     Title = "Bacon Cheese Spread with Carmelized Onions",
                     Description = @"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    Image = "",
+                    Image = @"/Assets/seafood_dish.jpg",
                     Notes = @"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. "
                 }
             };
