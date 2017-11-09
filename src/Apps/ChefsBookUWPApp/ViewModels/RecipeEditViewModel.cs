bb6f2b2ba0134bd7e5d755610a08b2f09466c58d
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2017-11-09 21:53:57 +0100

    Finish recipe details editing
    Related Work Items: #119

--- src/Apps/ChefsBookUWPApp/ViewModels/RecipeEditViewModel.cs
+++ src/Apps/ChefsBookUWPApp/ViewModels/RecipeEditViewModel.cs
@@ -29,6 +29,62 @@ namespace ChefsBook_UWP_App.ViewModels
             set => Set(ref _recipe, value);
         }
 
+        private RelayCommand _addEmptyIngredientCommand;
+        public RelayCommand AddEmptyIngredientCommand
+        {
+            get
+            {
+                return _addEmptyIngredientCommand
+                    ?? (_addEmptyIngredientCommand = new RelayCommand(
+                    () =>
+                    {
+                        Recipe.Ingredients.Add(new IngredientViewModel());
+                    }));
+            }
+        }
+
+        private RelayCommand _addEmptyStepCommand;
+        public RelayCommand AddEmptyStepCommand
+        {
+            get
+            {
+                return _addEmptyStepCommand
+                    ?? (_addEmptyStepCommand = new RelayCommand(
+                    () =>
+                    {
+                        Recipe.Steps.Add(new StepViewModel());
+                    }));
+            }
+        }
+
+        private RelayCommand<IngredientViewModel> _deleteIngredientCommand;
+        public RelayCommand<IngredientViewModel> DeleteIngredientCommand
+        {
+            get
+            {
+                return _deleteIngredientCommand
+                    ?? (_deleteIngredientCommand = new RelayCommand<IngredientViewModel>(
+                    (ingredient) =>
+                    {
+                        Recipe.Ingredients.Remove(ingredient);
+                    }));
+            }
+        }
+
+        private RelayCommand<StepViewModel> _deleteStepCommand;
+        public RelayCommand<StepViewModel> DeleteStepCommand
+        {
+            get
+            {
+                return _deleteStepCommand
+                    ?? (_deleteStepCommand = new RelayCommand<StepViewModel>(
+                    (step) =>
+                    {
+                        Recipe.Steps.Remove(step);
+                    }));
+            }
+        }
+
         private RelayCommand _saveRecipeCommand;
         public RelayCommand SaveRecipeCommand
         {
