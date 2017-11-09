9ce6884dc82809c8efedea4ea3dff059aeeedb18
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2017-11-09 10:48:00 +0100

    Fix not working nullable types binding notification
    Related Work Items: #136

---
+++ src/Apps/ChefsBookUWPApp/Converters/NullableIntConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace ChefsBook_UWP_App.Converters
+{
+    public class NullableIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (string.IsNullOrEmpty((string)value))
+                return null;
+
+            int intValue = -1;
+            int.TryParse((string)value, out intValue);
+
+            if (intValue != -1)
+                return intValue;
+
+            return null;
+        }
+    }
+}
