f698cc9e82c71b4aa67ae5c439918da0ee8b6324
Bartosz Selwesiuk <bartek@leancode.pl>
2017-11-15 21:49:41 +0100

    Add create recipe tests

--- tests/Apps/WebApiApp.Tests/Controllers/RecipesControllerTests.cs
+++ tests/Apps/WebApiApp.Tests/Controllers/RecipesControllerTests.cs
@@ -76,9 +76,9 @@ namespace ChefsBook.WebApiApp.Tests
             // Arrange
             var recipesService = Substitute.For<IRecipesService>();
             var controller = new RecipesController(recipesService, AutoMapper.Mapper.Instance);
+            var filter = new FilterRecipeDTO { Text = null, Tags = null };
 
             // Act
-            var filter = new FilterRecipeDTO { Text = null, Tags = null };
             var result = await controller.FilterRecipes(filter);
 
             // Assert
@@ -88,13 +88,47 @@ namespace ChefsBook.WebApiApp.Tests
         [Fact]
         public async Task CreateRecipe_Returns_Ok_When_Arguments_Are_Valid()
         {
+            // Arrange
+            var recipesService = Substitute.For<IRecipesService>();
+            recipesService
+                .Create(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>(), Arg.Any<TimeSpan?>(), Arg.Any<int?>(), Arg.Any<string>(), Arg.Any<IList<Ingredient>>(), Arg.Any<IList<Step>>(), Arg.Any<IList<Tag>>())
+                .Returns(Task.CompletedTask);
+            var controller = new RecipesController(recipesService, AutoMapper.Mapper.Instance);
+            var recipe = new NewRecipeDTO { 
+                Title = "Naleśniki", 
+                Description = "Naleśniki z serem", 
+                Image = null,
+                Duration = TimeSpan.FromMinutes(30), 
+                Servings = 12,
+                Notes = "Idealne na lekki głód",
+                Ingredients = new List<NewRecipeIngredientDTO>(),
+                Steps = new List<NewRecipeStepDTO>(),
+                Tags = new List<NewRecipeTagDTO>()
+            };
 
+            // Act
+            var result = await controller.CreateRecipe(recipe);
+
+            // Assert
+            Assert.True(result.GetType().IsAssignableFrom(typeof(OkResult)));
         }
 
         [Fact]
-        public async Task CreateRecipe_Throws_ArgumentException_When_Recipe_Details_Are_Invalid()
+        public async Task CreateRecipe_Returns_BadRequest_When_Arguments_Are_Invalid()
         {
+            // Arrange
+            var recipesService = Substitute.For<IRecipesService>();
+            recipesService
+                .Create(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>(), Arg.Any<TimeSpan?>(), Arg.Any<int?>(), Arg.Any<string>(), Arg.Any<IList<Ingredient>>(), Arg.Any<IList<Step>>(), Arg.Any<IList<Tag>>())
+                .Returns(Task.CompletedTask);
+            var controller = new RecipesController(recipesService, AutoMapper.Mapper.Instance);
+            var recipe = new NewRecipeDTO();
 
+            // Act
+            var result = await controller.CreateRecipe(recipe);
+
+            // Assert
+            Assert.True(result.GetType().IsAssignableFrom(typeof(BadRequestResult)));
         }
 
         private List<Recipe> GenerateRecipes()
