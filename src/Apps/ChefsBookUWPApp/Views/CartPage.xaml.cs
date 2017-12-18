9101c9485ec036e77acea414e42f6abab430a47f
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2017-12-18 12:49:28 +0100

    Add copy cart to clipboard option

--- src/Apps/ChefsBookUWPApp/Views/CartPage.xaml.cs
+++ src/Apps/ChefsBookUWPApp/Views/CartPage.xaml.cs
@@ -2,7 +2,9 @@
 using Microsoft.Practices.ServiceLocation;
 using System.Text;
 using Windows.ApplicationModel.DataTransfer;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -30,6 +32,22 @@ namespace ChefsBook_UWP_App.Views
 
             if (e.Parameter != null && (bool)e.Parameter == true)
                 ViewModel.GetCartItems();
+        }
+
+        private void CopyCartContentAppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
+
+            var dataPackage = new DataPackage();
+            var builder = new StringBuilder();
+
+            foreach (var item in ViewModel.Items)
+            {
+                builder.AppendLine($"{item.Name} [{item.Quantity}]");
+            }
+
+            dataPackage.SetText(builder.ToString());
+            Clipboard.SetContent(dataPackage);
         }
     }
 }
