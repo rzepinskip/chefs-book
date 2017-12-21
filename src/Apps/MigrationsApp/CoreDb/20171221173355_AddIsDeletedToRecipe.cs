38af2b8cfb00e1efa844948ad581bd2e6e6ee0e1
Bartosz Selwesiuk <bartek@leancode.pl>
2017-12-21 19:19:12 +0100

    Add IsDeleted in recipe migration

---
+++ src/Apps/MigrationsApp/CoreDb/20171221173355_AddIsDeletedToRecipe.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ChefsBook.MigrationsApp.Migrations.CoreDb
+{
+    public partial class AddIsDeletedToRecipe : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                schema: "core",
+                table: "Recipes",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                schema: "core",
+                table: "Recipes");
+        }
+    }
+}
