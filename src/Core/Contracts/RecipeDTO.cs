43bcd89e85bd28b30491792117fc15740f415a4d
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2018-01-20 00:30:02 +0100

    Modify RecipeDTo to return UserId in detailed recipe

--- src/Core/Contracts/RecipeDTO.cs
+++ src/Core/Contracts/RecipeDTO.cs
@@ -6,6 +6,7 @@ namespace ChefsBook.Core.Contracts
     public class RecipeDTO
     {
         public Guid Id { get; set; }
+        public Guid UserId { get; set; }
         public string Title { get; set; }
         public bool IsPrivate { get; set; }
         public string Description { get; set; }
